c9e8a9ed982447a38dc3d24b24b1c7813c6bb03b
ElenaKildishova <elenanov@ngs.ru>
2018-09-13 16:52:05 +0700

    Добавлено несколько тестов, которые проверяют, что расстояние между двумя точками вычисляется правильно

--- sandbox/src/test/java/ru/p4b/dev/sandbox/DistanceTests.java
+++ sandbox/src/test/java/ru/p4b/dev/sandbox/DistanceTests.java
@@ -6,9 +6,21 @@ import org.testng.annotations.Test;
 public class DistanceTests {
 
   @Test
-  public void distance() {
+  public void testDistance1() {
     Point p1 = new Point(6, 7);
     Point p2 = new Point(9,10);
     Assert.assertEquals(p1.distance(p2), 4.242640687119285);
+  }
+  @Test
+  public void testDistance2() {
+    Point p1 = new Point(-1, -1);
+    Point p2 = new Point(-1,-1);
+    Assert.assertEquals(p1.distance(p2), 0.0);
+  }
+  @Test
+  public void testDistance3() {
+    Point p1 = new Point(-11, 887);
+    Point p2 = new Point(9,0);
+    Assert.assertEquals(p1.distance(p2), 887.2254504915873);
   }
 }
