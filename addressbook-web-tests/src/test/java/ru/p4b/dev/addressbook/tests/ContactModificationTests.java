58eab69e68972c3007ef7f6c98656056c9ca59c1
ElenaKildishova <elenanov@ngs.ru>
2018-09-20 19:12:33 +0700

    Добавлен тест для модификации контактов

---
+++ addressbook-web-tests/src/test/java/ru/p4b/dev/addressbook/tests/ContactModificationTests.java
@@ -0,0 +1,17 @@
+package ru.p4b.dev.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.p4b.dev.addressbook.model.ContactData;
+
+public class ContactModificationTests extends TestBase {
+
+  @Test
+  public void testContactModification() {
+    app.getNavigationHelper().gotoHomePage();
+    app.getContactHelper().selectContact();
+    app.getContactHelper().initContactModification();
+    app.getContactHelper().fillContactForm(new ContactData("E", "K", "+79130000000", "elenanova@ngs.ru"));
+    app.getContactHelper().submitContactModification();
+    app.getContactHelper().returnToHomePage();
+  }
+}
