e00d8728c350a554a5f1dd6084c23989e5ac4027
ElenaKildishova <elenanov@ngs.ru>
2018-09-24 18:20:35 +0700

    Реализована возможность запускать тесты в разных браузерах: FireFox, Chrome and IE

--- addressbook-web-tests/src/test/java/ru/p4b/dev/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/p4b/dev/addressbook/appmanager/ApplicationManager.java
@@ -3,7 +3,10 @@ package ru.p4b.dev.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -17,9 +20,21 @@ public class ApplicationManager {
   private GroupHelper groupHelper;
   public boolean acceptNextAlert = true;
   StringBuffer verificationErrors = new StringBuffer();
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void init() {
-    wd = new FirefoxDriver();
+    if (browser.equals(BrowserType.FIREFOX)) {
+      wd = new FirefoxDriver();
+    } else if (browser.equals(BrowserType.CHROME)) {
+      wd = new ChromeDriver();
+    } else if (browser.equals(BrowserType.IE)) {
+      wd = new InternetExplorerDriver();
+    }
+
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost:8080/addressbook/group.php");
     groupHelper = new GroupHelper(wd);
--- addressbook-web-tests/src/test/java/ru/p4b/dev/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/p4b/dev/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package ru.p4b.dev.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.p4b.dev.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
