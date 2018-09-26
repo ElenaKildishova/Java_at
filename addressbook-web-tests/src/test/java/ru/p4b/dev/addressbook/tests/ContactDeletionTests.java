b4fa5b94ab9fb9cafd56b553c0e66d1558ec3173
ElenaKildishova <elenanov@ngs.ru>
2018-09-26 17:26:31 +0700

    Реализована проверка и обеспечение выполнения предусловий для удаления и модификации контактов

--- addressbook-web-tests/src/test/java/ru/p4b/dev/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/p4b/dev/addressbook/tests/ContactDeletionTests.java
@@ -1,12 +1,16 @@
 package ru.p4b.dev.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.p4b.dev.addressbook.model.ContactData;
 
 public class ContactDeletionTests extends TestBase {
 
   @Test
   public void testContactDeletion() throws Exception {
     app.getNavigationHelper().gotoHomePage();
+    if (! app.getContactHelper().isThereAContact()) {
+     app.getContactHelper().createContact(new ContactData("Elena", "Kildishova", "+79139507792", "elenanov@ngs.ru", "[none]"), true);
+    }
     app.getContactHelper().selectContact();
     app.acceptNextAlert = true;
     app.getContactHelper().deleteSelectedContacts();
--- addressbook-web-tests/src/test/java/ru/p4b/dev/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/p4b/dev/addressbook/tests/ContactModificationTests.java
@@ -8,9 +8,12 @@ public class ContactModificationTests extends TestBase {
   @Test
   public void testContactModification() {
     app.getNavigationHelper().gotoHomePage();
+    if (! app.getContactHelper().isThereAContact()) {
+      app.getContactHelper().createContact(new ContactData("Elena", "Kildishova", "+79139507792", "elenanov@ngs.ru", "[none]"), true);
+    }
     app.getContactHelper().selectContact();
     app.getContactHelper().initContactModification();
-    app.getContactHelper().fillContactForm(new ContactData("E", "K", "+79130000000", "elenanova@ngs.ru", null), false);
+    app.getContactHelper().fillContactForm(new ContactData("E", "K", "+79130000000", "elenanova@ngs.ru", "[none]"), false);
     app.getContactHelper().submitContactModification();
     app.getContactHelper().returnToHomePage();
   }
