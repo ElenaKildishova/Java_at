d91de3590e6121849bdb30c1e8a2ce8e9e2206da
ElenaKildishova <elenanov@ngs.ru>
2018-10-15 19:44:08 +0700

    Реализован тест для проверки телефонов, почтового адреса и адресов электронной почты на главной странице приложения.

---
+++ addressbook-web-tests/src/test/java/ru/p4b/dev/addressbook/tests/ContactDataTests.java
@@ -0,0 +1,56 @@
+package ru.p4b.dev.addressbook.tests;
+
+import org.hamcrest.CoreMatchers;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import ru.p4b.dev.addressbook.model.ContactData;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ContactDataTests extends TestBase {
+
+  @BeforeMethod
+  public void ensurePreconditions() {
+    app.goTo().homePage();
+    if (app.contact().all().size() == 0) {
+      app.contact().create(new ContactData()
+              .withFirstName("C").withLastName("D").withHomePhone("+333-33-33").withMobilePhone("+791395 0000")
+              .withWorkPhone("8(383)45456").withEmail("SSS@ngs.ru").withEmail2("222@mail.ru").withEmail3("33_3@process4.biz")
+              .withAddress("630055 г.Новосибирск, ул. Восход д.55, кв1").withGroup("[none]"), true);
+    }
+  }
+
+  @Test
+  public void testContactData() {
+    app.goTo().homePage();
+    ContactData contact = app.contact().all().iterator().next();
+    ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+    assertThat(contact.getAllPhones(), equalTo(mergePhones(contactInfoFromEditForm)));
+    assertThat(contact.getAllEmails(), equalTo(mergeEmails(contactInfoFromEditForm)));
+    assertThat(contact.getAddress(), equalTo(contactInfoFromEditForm));
+  }
+
+  private String mergePhones(ContactData contact) {
+    return Arrays.asList(contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone())
+            .stream().filter((s) -> ! s.equals(""))
+            .map(ContactDataTests::cleaned).collect(Collectors.joining("\n"));
+
+  }
+
+  private String mergeEmails(ContactData contact) {
+    return Arrays.asList(contact.getEmail(), contact.getEmail2(), contact.getEmail3())
+            .stream().filter((s) -> ! s.equals(""))
+            .map(ContactDataTests::cleaned).collect(Collectors.joining("\n"));
+
+
+  }
+
+
+  public static String cleaned(String phone) {
+    return phone.replaceAll("\\s", "").replaceAll("[-()]", "");
+  }
+}
