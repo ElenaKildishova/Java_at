8f5346833410ed188f1e1e228b176e0a3af1a526
ElenaKildishova <elenanov@ngs.ru>
2018-11-03 14:59:42 +0700

    Добавлен новый модуль для тестов Mantis

---
+++ mantis-tests/src/test/java/ru/p4b/dev/mantis/appmanager/ApplicationManager.java
@@ -0,0 +1,54 @@
+package ru.p4b.dev.mantis.appmanager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.fail;
+
+public class ApplicationManager {
+  private final Properties properties;
+  public WebDriver wd;
+
+  public boolean acceptNextAlert = true;
+  StringBuffer verificationErrors = new StringBuffer();
+  private String browser;
+
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+    properties = new Properties();
+  }
+
+  public void init() throws IOException {
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+    if (browser.equals(BrowserType.FIREFOX)) {
+      wd = new FirefoxDriver();
+    } else if (browser.equals(BrowserType.CHROME)) {
+      wd = new ChromeDriver();
+    } else if (browser.equals(BrowserType.IE)) {
+      wd = new InternetExplorerDriver();
+    }
+
+    wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+    wd.get(properties.getProperty("web.baseUrl"));
+  }
+
+  public void stop() {
+    wd.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+}
